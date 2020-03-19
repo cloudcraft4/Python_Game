0ce46e39d5ca33ca784143b7ee4397fde9cf4107
cloudcraft4 <43050448+cloudcraft4@users.noreply.github.com>
2020-03-19 15:12:01 -0600

    Early work on making skills actually do something

---
+++ skill_functions.py
@@ -0,0 +1,58 @@
+import libtcodpy as libtcod
+
+from game_messages import Message
+
+
+def cast_firebreath(*args, **kwargs):
+    caster = args[0]
+    entities = kwargs.get('entities')
+    fov_map = kwargs.get('fov_map')
+    damage = kwargs.get('damage')
+    maximum_range = kwargs.get('maximum_range')
+
+    results = []
+
+    target = None
+    closest_distance = maximum_range + 1
+
+    for entity in entities:
+        if entity.fighter and entity != caster and libtcod.map_is_in_fov(fov_map, entity.x, entity.y):
+            distance = caster.distance_to(entity)
+
+            if distance < closest_distance:
+                target = entity
+                closest_distance = distance
+
+    if target:
+        results.append({'consumed': True, 'target': target, 'message': Message('A bolt of fire strikes the {0} with a boom! The damage is {1}'.format(target.name, damage))})
+        results.extend(target.fighter.take_damage(damage))
+    else:
+        results.append({'consumed': False, 'target': None, 'message': Message('No enemy is close enough to strike.', libtcod.red)})
+
+    return results
+
+def cast_thorns(*args, **kwargs):
+    caster = args[0]
+    attacker = kwargs.get('attacker')
+    damage = kwargs.get('damage')
+
+    results = []
+
+    '''if not libtcod.map_is_in_fov(fov_map, target_x, target_y):
+        results.append({'consumed': False, 'message': Message('You cannot target a tile outside your field of view.', libtcod.yellow)})
+        return results
+
+    for entity in entities:
+        if entity.x == target_x and entity.y == target_y and entity.ai:
+            confused_ai = ConfusedMonster(entity.ai, 10)
+
+            confused_ai.owner = entity
+            entity.ai = confused_ai
+
+            results.append({'consumed': True, 'message': Message('The eyes of the {0} look vacant, as he starts to stumble around!'.format(entity.name), libtcod.light_green)})
+
+            break
+    else:
+        results.append({'consumed': False, 'message': Message('There is no targetable enemy at that location.', libtcod.yellow)})
+
+    return results'''
